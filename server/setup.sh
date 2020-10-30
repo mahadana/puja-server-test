2a71eef2e8c3365d8c9f750013c5d37eb0de9c51
Jāgaro <v.jagaro@gmail.com>
2020-10-30 13:02:36 -0700

    Add nginx

--- server/setup.sh
+++ server/setup.sh
@@ -12,12 +12,6 @@ test -d /opt/puja-server-test || \
 
 cd /opt/puja-server-test
 
-apt-get install -y webhook
-cp server/webhook.conf /etc/webhook.conf
-perl -pi -e 's/SECRET/mysecret/' /etc/webhook.conf
-# TODO ask for and manipulate secret
-systemctl restart webhook.service
-
 test -f .env || cat <<END > .env
 DB_PORT=5432
 DB_DATABASE=test
@@ -28,3 +22,23 @@ END
 docker-compose down
 git pull
 docker-compose up -d
+
+test -x /usr/sbin/nginx || apt-get install -y nginx-light
+test -x /usr/bin/certbot || apt-get install -y certbot
+test -d /usr/share/doc/python3-certbot-nginx || apt-get install -y python3-certbot-nginx
+
+certbot --nginx --non-interactive --agree-tos --email admin@pujas.live \
+  --domain 'pujas.live' \
+  --domain 'www.pujas.live' \
+  --domain 'puja-server-test.pujas.live'
+
+cp server/nginx.conf /etc/nginx/sites-available/puja-server-test
+ln -sf ../sites-available/puja-server-test \
+  /etc/nginx/sites-enabled/puja-server-test
+systemctl restart nginx.service
+
+test -x /usr/bin/webhook || apt-get install -y webhook
+cp server/webhook.conf /etc/webhook.conf
+perl -pi -e 's/SECRET/mysecret/' /etc/webhook.conf
+# TODO ask for and manipulate secret
+systemctl restart webhook.service
