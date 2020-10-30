477be9751147155b5cbb1f0ba281eea41a837ca2
Jāgaro <v.jagaro@gmail.com>
2020-10-30 16:40:15 -0700

    Fast deploy return

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
+set -eu
 
-cd /opt/puja-server-test
-git pull
-docker-compose build --no-cache
-docker-compose rm -fs app
-docker-compose -f docker-compose.yml -f docker-compose.server.yml up -d
-docker image prune -f
+lock="/var/lock/puja-server-test-deploy.lock"
+log="/var/log/puja-server-test-deploy.log"
+
+(
+  flock -n 9 || exit 1
+
+  echo "$(date) start puja-server-test deploy"
+
+  cd /opt/puja-server-test
+  git pull
+  docker-compose build --no-cache
+  docker-compose rm -fs app
+  docker-compose -f docker-compose.yml -f docker-compose.server.yml up -d
+  docker image prune -f
+
+  echo "$(date) end puja-server-test deploy"
+
+) >>"$log" 2>&1 9>"$lock" &
